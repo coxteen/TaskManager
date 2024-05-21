9ddde52c2b0bf4999212828f2c90c13b40c2b73f
coxteen <costin.ghiujan@gmail.com>
2024-05-21 18:09:44 +0300

    verif

--- window.py
+++ window.py
@@ -4,10 +4,10 @@ from screeninfo import get_monitors
 window = windows.CTk()
 
 WIDTH_CONSTANT = 600
-H_CONSTANT = W_CONSTANT - 200
+HEIGHT_CONSTANT = WIDTH_CONSTANT - 200
 
-width = get_monitors()[0].width - W_CONSTANT
-height = get_monitors()[0].height - H_CONSTANT
+width = get_monitors()[0].width - WIDTH_CONSTANT
+height = get_monitors()[0].height - HEIGHT_CONSTANT
 
 window.geometry('{}x{}'.format(width, height))
 window.title('Task Manager')
