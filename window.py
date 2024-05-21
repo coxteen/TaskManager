148b0a831b13cc75f2d2e0f432a0f4e061f341cb
coxteen <costin.ghiujan@gmail.com>
2024-05-21 18:16:23 +0300

    verif

--- window.py
+++ window.py
@@ -1,18 +1,21 @@
-from customtkinter import windows
-from screeninfo import get_monitors
+# from customtkinter import windows
+# from screeninfo import get_monitors
+#
+# window = windows.CTk()
+#
+# WIDTH_CONSTANT = 600
+# HEIGHT_CONSTANT = WIDTH_CONSTANT - 200
+#
+# width = get_monitors()[0].width - WIDTH_CONSTANT
+# height = get_monitors()[0].height - HEIGHT_CONSTANT
+#
+# window.geometry('{}x{}'.format(width, height))
+# window.title('Task Manager')
+#
+# window.mainloop()
 
-window = windows.CTk()
-
-WIDTH_CONSTANT = 600
-HEIGHT_CONSTANT = WIDTH_CONSTANT - 200
-
-width = get_monitors()[0].width - WIDTH_CONSTANT
-height = get_monitors()[0].height - HEIGHT_CONSTANT
-
-window.geometry('{}x{}'.format(width, height))
-window.title('Task Manager')
-
-window.mainloop()
+import tkinter as tk
+from system_info import get_processor_frequency
 
 def create_window():
     root = tk.Tk()
