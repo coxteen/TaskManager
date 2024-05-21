7e9e60960f4fd3e9382f086a2d4ccfd9d2656a68
coxteen <costin.ghiujan@gmail.com>
2024-05-21 18:22:03 +0300

    verif

--- window.py
+++ window.py
@@ -1,31 +1,19 @@
-# from customtkinter import windows
-# from screeninfo import get_monitors
-#
-# window = windows.CTk()
-#
-# WIDTH_CONSTANT = 600
-# HEIGHT_CONSTANT = WIDTH_CONSTANT - 200
-#
-# width = get_monitors()[0].width - WIDTH_CONSTANT
-# height = get_monitors()[0].height - HEIGHT_CONSTANT
-#
-# window.geometry('{}x{}'.format(width, height))
-# window.title('Task Manager')
-#
-# window.mainloop()
+from customtkinter import windows
+from screeninfo import get_monitors
 
-import tkinter as tk
-from system_info import get_processor_frequency
 
 def create_window():
-    root = tk.Tk()
-    root.title("Processor Frequency")
+    WIDTH_CONSTANT = 700
+    HEIGHT_CONSTANT = WIDTH_CONSTANT - 200
 
-    frequency = get_processor_frequency()
-    label = tk.Label(root, text=f"Processor Frequency: {frequency:.2f} MHz")
-    label.pack(padx=20, pady=20)
+    width = get_monitors()[0].width - WIDTH_CONSTANT
+    height = get_monitors()[0].height - HEIGHT_CONSTANT
 
-    root.mainloop()
+    window = windows.CTk()
 
-if __name__ == "__main__":
-    create_window()
+    window.geometry('{}x{}'.format(width, height))
+    window.title('Task Manager')
+
+    window.mainloop()
+
+create_window()
